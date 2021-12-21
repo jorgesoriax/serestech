5bad2e44acdfd87712873b1166d272031434bde8
Jorge Soria <jorge_soriax@outlook.com>
2021-12-20 23:55:08 -0600

    Corrección y adiciones en librería y ficheros

---
+++ app/js/scripts.js
@@ -0,0 +1,18 @@
+const showHiddenPass = (inputPass, iconPass) => {
+    const input = document.getElementById(inputPass),
+          icon = document.getElementById(iconPass)
+
+    icon.addEventListener('click', () => {
+        if(input.type === 'password'){
+            input.type = 'text';
+
+            icon.classList.add('bxs-show');
+        } else {
+            input.type = 'password';
+
+            icon.classList.remove('bxs-show');
+        }
+    })
+}
+
+showHiddenPass('input--pass','icon--pass');
